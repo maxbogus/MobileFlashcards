2dab1179ac7ba2ce30ec66e76dd65a5c02ed5980
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-03 01:46:55 +0300

    - implemented draft deck list component;

---
+++ components/ListDecks.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class ListDecks extends Component {
+    render() {
+        return (
+            <View>
+                <Text>List of Decks</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
