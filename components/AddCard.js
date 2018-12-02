c35fceb084a3b7253a7bb4c643aa80d5a2775b39
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-03 01:45:29 +0300

    - implemented draft add card component;

---
+++ components/AddCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class AddCard extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Add Card</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
