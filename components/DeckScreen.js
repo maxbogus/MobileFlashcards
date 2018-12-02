387c39a2b2d700f748977a95c73f6cedc67775ad
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-03 01:47:32 +0300

    - implemented draft deck screen component;

---
+++ components/DeckScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class DeckScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Deck screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
