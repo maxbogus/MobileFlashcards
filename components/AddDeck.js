387cab4dffd142556462c3821bb8092960115dd2
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-03 01:46:11 +0300

    - implemented draft add deck component;

---
+++ components/AddDeck.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class AddDeck extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Add Deck</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
