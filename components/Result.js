d6767f8bbca9c67224fd9fa19a8c38347f355095
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-04 01:37:06 +0300

    added draft of result component

---
+++ components/Result.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class Result extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Result</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
