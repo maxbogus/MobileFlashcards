ffe16da22f2b977a1172c8046cd68913cd8d4004
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-04 01:36:46 +0300

    added draft of quiz component

---
+++ components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+
+class Quiz extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Quiz</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({});
