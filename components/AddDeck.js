fdcc00ff7c3a41d77c3ff45a39e5ff048363ba79
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-05 01:12:27 +0300

    implemented draft logic of add deck component

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native'
 
 export default class AddDeck extends Component {
+    state = {
+        text: ''
+    };
+
+    submitHandler = () => {
+        console.log(this.state.text)
+    };
+
     render() {
         return (
             <View>
-                <Text>Add Deck</Text>
+                <Text>What is the title of your new deck?</Text>
+                <TextInput value={this.state.text}
+                           onChangeText={(text) => this.setState({text})}/>
+                <Button disabled={this.state.text === ''}
+                        title='Submit'
+                        onPress={() => this.submitHandler()}/>
             </View>
         )
     }
