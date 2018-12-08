f030a22b6fb838d7efc8d89160cda0398d7a9ba8
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-09 02:57:16 +0300

    reworked storage

--- utils/storage.js
+++ utils/storage.js
@@ -9,18 +9,25 @@ export function getDecks() {
 // getDeck: take in a single id argument and return the deck associated with that id.
 export function getDeck(id) {
     return AsyncStorage.getItem(STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            return data[id]
+        })
 }
 
 // saveDeckTitle: take in a single title argument and add it to the decks.
 export function saveDeckTitle({title}) {
-    return AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify({
-        title,
-    }))
+    return AsyncStorage.getItem(STORAGE_KEY)
+        .then((results) => {
+            const data = JSON.parse(results);
+            data[title] = undefined;
+            AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+        })
 }
 
 // addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
 export function addCardToDeck({title, card}) {
     return AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify({
-        title: card,
+        [title]: card,
     }))
 }
