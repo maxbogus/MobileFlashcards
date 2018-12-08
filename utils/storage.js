70a29b78cda17fb6232b5fbf073eb258c90a25f4
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-08 15:17:56 +0300

    - added comments to async storage helper methods.

--- utils/storage.js
+++ utils/storage.js
@@ -1,12 +1,26 @@
 import {AsyncStorage} from 'react-native'
-import {STORAGE_KEY} from "./_decks";
+import {STORAGE_KEY} from './_decks'
 
-export function fetchDecks() {
+// getDecks: return all of the decks along with their titles, questions, and answers.
+export function getDecks() {
     return AsyncStorage.getItem(STORAGE_KEY)
 }
 
-export function submitDeck({entry, key}) {
+// getDeck: take in a single id argument and return the deck associated with that id.
+export function getDeck(id) {
+    return AsyncStorage.getItem(STORAGE_KEY)
+}
+
+// saveDeckTitle: take in a single title argument and add it to the decks.
+export function saveDeckTitle({title}) {
     return AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify({
-        [key]: entry,
+        title,
+    }))
+}
+
+// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title.
+export function addCardToDeck({title, card}) {
+    return AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify({
+        title: card,
     }))
 }
