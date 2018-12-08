5340b868869175a1e01382b2e0795970cfc24b7e
Max Boguslavskiy <max.bogus@gmail.com>
2018-12-08 15:08:58 +0300

    - added question action.

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,6 @@
 export const RECIEVE_DECKS = 'RECIEVE_DECKS';
 export const ADD_DECK = 'ADD_DECK';
+export const ADD_QUESTION = 'ADD_QUESTION';
 
 export function recieveDecks(decks) {
     return {
